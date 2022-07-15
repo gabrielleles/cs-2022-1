7596967e701ca476f4e56fd11f6f7aed54ccd431
Gabriel Leles Pires Lopes <gabrielleles1609@gmail.com>
2022-07-15 19:53:31 -0300

    Delete tarefa 008/endereco directory

--- tarefa 008/endereco/src/main/java/br/com/aula08/endereco/Bairro.java
+++
@@ -1,6 +0,0 @@
-package br.com.aula08.endereco;
-
-public class Bairro {
-	 private String nome;
-	 Cidade cidade;    
-}
--- tarefa 008/endereco/src/main/java/br/com/aula08/endereco/TipoEndereco.java
+++
@@ -1,9 +0,0 @@
-package br.com.aula08.endereco;
-
-public enum TipoEndereco {
-	
-	 COMERCIAL,
-	 RESIDENCIAL;   
-	 ArrayList<TipoEndereco> enderecos = new ArrayList<>();    
-
-}
